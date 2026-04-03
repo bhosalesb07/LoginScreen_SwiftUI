8871fe7ca9094ee4a4c6af978601662d529e1276
bhosalesb07 <bhosalesb07@gmail.com>
2026-04-03 19:01:16 +0530

    Refactored code

---
+++ LoginScreen_SwiftUI/InputFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  InputFieldView.swift
+//  LoginScreen_SwiftUI
+//
+//  Created by Swapnil B on 17/03/26.
+//
+
+import SwiftUI
+
+struct InputFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputFieldView()
+}
--- LoginScreen_SwiftUI/LoginScreen.swift
+++ LoginScreen_SwiftUI/LoginScreen.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginScreen: View {
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +22,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LoginScreen()
 }
